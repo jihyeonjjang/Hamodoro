805861db604a159e492375cccf40ecb492968566
jihyeonjjang <jihyeon9975@gmail.com>
2026-05-11 13:27:05 +0900

    feat: create macOS SwiftUI app project

---
+++ Hamodoro/Hamodoro/HamodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  HamodoroApp.swift
+//  Hamodoro
+//
+//  Created by 지현 on 5/11/26.
+//
+
+import SwiftUI
+
+@main
+struct HamodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
